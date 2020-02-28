3b1825fcb2d6b6b6e6bac00a0040e8a0baa7fa5f
Rachele Edwards <dot.com1472@gmail.com>
2020-02-28 08:58:47 -0700

    worked on practice

--- challenges/functions.js
+++ challenges/functions.js
@@ -10,7 +10,7 @@ The third will return the modulus of the two numbers.
 The fourth will return the quotient of the two numbers.
 The fifth will return the square root of the two numbers.
 The sixth will accept a first and last name and return 'Hey, firstName lastName, youre a wicked cool dev'.
-The seventh will return whether the firstString is included the secondString.
+The seventh will return whether the firstString is included the secondString. ????
 The eigth will return whether the firstNumber is greater than the secondNumber.
 The ninth will return whether the firstNumber is less than the secondNumber.
 The tenth will return whether the firstNumber is greater than or equal to the secondNumber.
@@ -20,3 +20,94 @@ The thirteenth will return a string 'Hey the number is firstNumber' if firstNumb
 */
 
 // Step 3: Check your work using console.log and invoke your higher order function.
+function one(num1, num2){
+    return num1 + num2;
+}
+console.log(one(1,2));
+
+function two(num1, num2){
+    return num1 * num2;
+}
+console.log(two(1,2));
+
+function three(num1, num2){
+    return num1 % num2;
+}
+console.log(three(10, 2));
+
+function four(num1, num2){
+    return quotient = num1 / num2;
+}
+console.log(four(4,2));
+
+function five(num1, num2){
+    total = num1 + num2;
+    return Math.sqrt(total);
+}
+console.log(five(2,2));
+
+function six(firstName, lastName){
+    return `Hey, ${firstName} ${lastName}, youre a wicked cool dev.`;
+}
+console.log(six('Rachele','Edwards'));
+
+function seven(string1, string2){
+    if(string1.includes(string2)){
+        return true;
+    }
+}
+console.log(one('Hello my name is ', 'rachele'));
+
+function eight(num1, num2){
+    if(num1>num2){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(eight(1,2));
+
+function nine(num1, num2){
+    if(num1<num2){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(nine(1,2));
+
+function ten(num1, num2){
+    if(num1>=num2){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(ten(1,2));
+
+function eleven(num1, num2){
+    if(num1<=num2){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(eleven(1,2));
+
+function twelve(num1, num2){
+    if(num1===num2){
+        return true;
+    }else{
+      return false;
+    }
+}
+console.log(twelve(1,2));
+
+function thirteen(num1, num2){
+    if(num1>num2){
+        return `Hey the number is ${num1}`;
+    }else{
+        return `Hey the number is ${num2}`;
+    }
+}
+console.log(thirteen(1,2));
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -2,7 +2,7 @@
 
 // Task: You are to build a pentagonal pyramid that can return values for its volume as well as its surface areas. Follow the steps to achieve this.
 
-// Step 1: Base Constructor -- You should create a constructor function names PentagonalPyramid that will accept properties for its base edge, and heights.
+// Step 1: Base Constructor -- You should create a constructor function names PentagonalPyramid that will accept properties for its base edge, and heights.✅
 
 // Step 2: Volume Method -- You should create a prototype for PentagonalPyramid that returns the volume of a pentagonal pyramid. Curious about the formula??? Use Google (A DEVELOPERS BEST FRIEND)
 
@@ -11,3 +11,19 @@
 // Step 4: New Object -- You should create a new object that uses PentagonalPyramid. Give it the length, width, and height.
 
 // Test your volume and surfaceArea methods here using console.log
+
+function PentagonalPyramid(input){
+    this.baseEdge = input.baseEdge,
+    this.height = input.height
+}
+PentagonalPyramid.prototype.volume = function(){
+    return volume = (5/12) * Math.tan(54) * this.height * Math.pow(this.baseEdge, 2);
+}
+
+const test = new PentagonalPyramid({
+    baseEdge: 5,
+    height: 10
+})
+
+console.log(test);
+console.log(test.volume());
